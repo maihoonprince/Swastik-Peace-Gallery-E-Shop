98741b539fbb288d5262daa93aedb5f2c89f9292
Prince <maihoonprinx@gmail.com>
2024-08-22 10:57:08 +0530

    Newsletter component Completed

--- client/src/components/Footer/Newsletter/Newsletter.jsx
+++ client/src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,7 +1,37 @@
+import React from "react";
+import {FaFacebookF, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+
 import "./Newsletter.scss";
 
 const Newsletter = () => {
-    return <div>Newsletter</div>;
+    return <div className="newsletter-section">
+        <div className="newsletter-content">
+            <span className="small-text">Newsletter</span>
+            <span className="big-text">Signup for latest updates and offer</span>
+            <div className="form">
+                <input type="text" placeholder="Email Address"/>
+                <button>Subscribe</button>
+            </div>
+            <div className="text">Will be used in accordance with our Privact Policy</div>
+            <div className="social-icons">
+                <div className="icon">
+                    <FaFacebookF size={14}/>
+                </div>
+
+                <div className="icon">
+                    <FaTwitter size={14} />
+                </div>
+
+                <div className="icon">
+                    <FaLinkedin size={14} />
+                </div>
+
+                <div className="icon">
+                    <FaInstagram size={14} />
+                </div>
+            </div>
+        </div>
+    </div>;
 };
 
 export default Newsletter;

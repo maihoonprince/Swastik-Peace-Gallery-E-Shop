7b7944139eb6273188af0aacfcdeaccf2c6a5c2f
Prince <maihoonprinx@gmail.com>
2024-08-25 13:48:21 +0530

    api.js file setup

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react";
+
 import "./Home.scss";
 
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
+import { fetchDataFromApi } from "../../utils/api";
 
 
 const Home = () => {
+
+    useEffect(() => {
+        getCategories()
+    }, [])
+
+    const getCategories = () => {
+        fetchDataFromApi("/api/categories?populate=*").then(res => console.log(res));
+    };
+
     return (
         <div>
             <Banner />

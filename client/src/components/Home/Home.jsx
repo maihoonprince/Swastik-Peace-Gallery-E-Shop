39ef2842ba3d82cc6badd6d41068398e1e84fa2e
Prince <maihoonprinx@gmail.com>
2024-08-29 15:35:18 +0530

    ...

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,36 +1,31 @@
-import React from "react";
-import { useEffect, useContext } from "react";
-
+import React, { useEffect, useContext } from "react";
 import "./Home.scss";
-
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
 import { fetchDataFromApi } from "../../utils/api";
 import { Context } from "../../utils/context";
 
-
 const Home = () => {
     const { categories, setCategories, products, setProducts } = useContext(Context);
+
+    // Define functions before they are used
+    const getProducts = async () => {
+        const res = await fetchDataFromApi("/api/products?populate=*");
+        console.log(res);
+        setProducts(res);
+    };
+
+    const getCategories = async () => {
+        const res = await fetchDataFromApi("/api/categories?populate=*");
+        console.log(res);
+        setCategories(res);
+    };
 
     useEffect(() => {
         getProducts();
         getCategories();
-    }, [getProducts, getCategories]);
-
-    const getProducts = () => {
-        fetchDataFromApi("/api/products?populate=*").then((res) => {
-            console.log(res);
-            setProducts(res);
-        });
-    };
-
-    const getCategories = () => {
-        fetchDataFromApi("/api/categories?populate=*").then((res) => {
-            console.log(res);
-            setCategories(res);
-        });
-    };
+    }, []); // Empty dependency array ensures this runs once after initial render
 
     return (
         <div>
